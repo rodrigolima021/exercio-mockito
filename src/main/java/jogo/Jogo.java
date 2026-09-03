7a7c1b720c1f46ca32558cd14b69c49808d57261
rodrigolima021 <165611836+rodrigolima021@users.noreply.github.com>
2026-09-03 12:57:01 -0300

    Update Jogo.java

--- src/main/java/jogo/Jogo.java
+++ src/main/java/jogo/Jogo.java
@@ -2,6 +2,14 @@ package jogo;
 
 public class Jogo {
 
+	/*
+	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 voc� ganha o jogo;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 voc� perde o jogo; 
+	 * Se n�o cair nenhum desses valores, o valor � armazenado e passa para o segundo turno;
+	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, voc� perde o jogo;
+	 * Se for o segundo turno, voc� continuando jogando os dados e s� ganha se cair um numero igual ao anterior do primeiro turno;
+	 */
+
     private final Jogador jogador;
     private final Dado dado1;
     private final Dado dado2;
@@ -12,8 +20,7 @@ public class Jogo {
         this.dado2 = dado2;
     }
 
-    public boolean jogo() {
-
+    public boolean jogar() {
         int resultado = jogador.lancar(dado1, dado2);
 
         if (resultado == 7 || resultado == 11) {
